1dbbf04ff0d9da5bb7c4796e88d32b2d97296013
Mika Rinne <mika.rinne@oracle.com>
2017-03-01 13:04:10 +0200

    deleted

--- kill.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-export id=$1
-
-echo "https://140.86.10.95/api/v2/containers/$id/kill"
-
-curl -sk -X "POST" -H "Authorization: Bearer a09332c280ddb89e" "https://140.86.10.95/api/v2/containers/$id/kill"
